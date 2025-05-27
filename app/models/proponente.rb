b41fdbb95a0f8a4de0595dc92513319200843ae8
AlxLanna <alexandre_lanna@hotmail.com>
2025-05-27 15:04:17 -0300

    model proponente aceita atributos aninhados de Endereços e Contatos

--- app/models/proponente.rb
+++ app/models/proponente.rb
@@ -2,4 +2,12 @@ class Proponente < ApplicationRecord
     # se um proponente for destruido, endereço e contato também são
     has_many :enderecos, dependent: :destroy
     has_many :contatos, dependent: :destroy
+
+    # Permite que o formulário de Proponente crie/atualize Enderecos associados,
+    # impede a criação de um endereço se todos os seus campos estiverem vazios e
+    # permite que endereços sejam removidos através do formulário.
+    accepts_nested_attributes_for :enderecos, reject_if: :all_blank, allow_destroy: true
+
+    # Permite que o formulário de Proponente crie/atualize Contatos associados.
+    accepts_nested_attributes_for :contatos, reject_if: :all_blank, allow_destroy: true
 end
